a73fe4f4aaf231ff2f9c59522438336dd8acf682
Sarah Ni <sni@Sarahs-MacBook-Air.local>
2015-10-17 15:19:42 +1100

    adding css

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.4'
 gem 'sqlite3'
 # classier solution for file uploads
 gem 'carrierwave'
+# imagemagick
+gem 'mini_magick', '3.8.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,8 +4,11 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
+  include CarrierWave::MiniMagick
+  # images uploaded from now on will be resized, pre-existing the same
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+  end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
